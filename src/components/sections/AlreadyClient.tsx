a54e8981333157f8d42199674ba245e3e3e1048a
alexfarkas05@gmail.com <lex@DESKTOP-83N5I69.>
2025-01-28 13:36:42 +0200

    updt

--- src/components/sections/AlreadyClient.tsx
+++ src/components/sections/AlreadyClient.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 function AlreadyClient() {
   return (
-    <section className="relative flex w-full max-w-[1400px] items-center justify-center p-5 lg:py-40">
+    <section className="relative flex w-full max-w-[1400px] items-center justify-center p-5 pt-40">
       <div className="flex w-full max-w-[1024px] flex-wrap items-center justify-center gap-5 rounded-3xl border border-neutral-500/50 p-5 md:justify-between md:p-14">
         <span className="text-center text-4xl font-bold">
           Already a customer?
